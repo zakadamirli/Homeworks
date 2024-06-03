3f2407f18a50930f755c596098d11764546895ad
zakadamirli <z3kkadamirli@gmail.com>
2024-06-03 20:26:42 +0400

    mentor lesson's onside task

---
+++ src/MentorDay05/Multiply.java
@@ -0,0 +1,11 @@
+package MentorDay05;
+public class Multiply {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            for (int j = 1; j < 11; j++) {
+                System.out.println(i + " * " + j + " = " + i * j);
+            }
+            System.out.println();
+        }
+    }
+}
