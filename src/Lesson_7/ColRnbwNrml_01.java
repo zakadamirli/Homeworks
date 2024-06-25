08a166069e3bd79c331d70af31518a77da0dd4a6
zakadamirli <z3kkadamirli@gmail.com>
2024-06-25 20:47:01 +0400

    Commit for lesson 6 homework

---
+++ src/Lesson_7/ColRnbwNrml_01.java
@@ -0,0 +1,10 @@
+package Lesson_7;
+
+public class ColRnbwNrml_01 {
+    public static void main(String[] args) {
+        String[]array={"Red","Orange","Yellow", "Green", "Blue", "Indigo", "Violet"};
+        for(String s:array){
+            System.out.println(s);
+        }
+    }
+}
