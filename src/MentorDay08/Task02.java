ad3776e758192d62330f70502e7657ee87d03b4d
zakadamirli <z3kkadamirli@gmail.com>
2024-06-28 19:21:39 +0400

    Homework commit Task2

---
+++ src/MentorDay08/Task02.java
@@ -0,0 +1,21 @@
+package MentorDay08;
+
+import java.util.Scanner;
+
+public class Task02 {
+    public static void main(String[] args) {
+        int sum=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Pls enter the array length");
+        int arrayLength=sc.nextInt();
+        int[] array=new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            System.out.println("Zehmet olmasa "+i+ " ci elementi daxil edin: ");
+            array[i]=sc.nextInt();
+        }
+        for (int i = 0; i <arrayLength ; i++) {
+            sum+=array[i];
+        }
+        System.out.println(sum);
+    }
+}
