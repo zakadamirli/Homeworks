6073c30abecec54cc112f8fa91b0a79db73a86fe
zakadamirli <z3kkadamirli@gmail.com>
2024-07-05 19:52:06 +0400

    Shape main method

---
+++ src/MentorDay10/RectangleClass.java
@@ -0,0 +1,13 @@
+package MentorDay10;
+
+public class RectangleClass {
+    public static void main(String[] args) {
+
+        double v = Shape.calculateArea();
+        System.out.println(v);
+        Shape.printShape();
+
+
+    }
+}
+
