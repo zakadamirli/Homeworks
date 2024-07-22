5af89f4e66fdd689c294f4ff261869d1d9d3e52b
zakadamirli <z3kkadamirli@gmail.com>
2024-07-22 19:35:18 +0400

    Pet class commit for Exercise Mentor Day

---
+++ src/MentorDay15/Exercise/Pet.java
@@ -0,0 +1,51 @@
+package MentorDay15.Exercise;
+
+public class Pet {
+    private String name;
+    private int age;
+    private double weight;
+
+    public Pet(String name, int age, double weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void eat() {
+        System.out.println(name + " is eating.");
+    }
+
+    public void sleep() {
+        System.out.println(name + " is sleeping.");
+    }
+
+    public void makeSound() {
+        System.out.println(name + " is making a sound.");
+    }
+}
+
+
