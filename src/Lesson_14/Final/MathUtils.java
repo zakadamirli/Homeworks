aba9beb65f3289ba9f1b94ff3bfd05e266052f04
zakadamirli <z3kkadamirli@gmail.com>
2024-07-23 19:31:13 +0400

    Commit for Homework Final keyword Main Lesson

---
+++ src/Lesson_14/Final/MathUtils.java
@@ -0,0 +1,10 @@
+package Lesson_14.Final;
+
+public class MathUtils {
+
+    public MathUtils() {
+    }
+    public final double add(double a, double b){
+        return a+b;
+    }
+}
---
+++ src/Lesson_14/Final/SubClass.java
@@ -0,0 +1,8 @@
+package Lesson_14.Final;
+
+public class SubClass extends MathUtils{
+    /*
+    Error: add(int,int) in SubClass cannot override add(int,int) in MathUtils
+    overridden method is final
+*/
+}
